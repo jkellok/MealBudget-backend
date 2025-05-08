9273ad0929111345989287c44b423718eefdfcde
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2025-05-08 16:34:06 +0300

    Expanding linked ingredients

--- src/routes/linked_ingredients.js
+++ src/routes/linked_ingredients.js
@@ -6,13 +6,18 @@ router.get('/', linkedIngredientService.getAll)
 
 router.get('/:recipe_id', linkedIngredientService.getLinkedIngredientsByRecipe)
 
+// change these?
+router.get('/:recipe_id/ingredient/:ingredient_id', linkedIngredientService.getLinkedIngredientCostPerKg)
+
 router.get('/:recipe_id/ingredient/:ingredient_id', linkedIngredientService.getOneLinkedIngredientByRecipe)
 
 router.get('/ingredient/:ingredient_id', linkedIngredientService.getRecipesByIngredient)
 
 router.post('/:recipe_id', linkedIngredientService.addLinkedIngredient)
 
-//router.put('/:id', linkedIngredientService.updateById)
+router.put('/:recipe_id/', linkedIngredientService.updateLinkedIngredient)
+
+router.put('/:recipe_id/ingredient/:ingredient_id', linkedIngredientService.updateLinkedIngredientCost)
 
 router.delete('/:recipe_id/ingredient/:ingredient_id', linkedIngredientService.deleteLinkedIngredientFromRecipe)
 
--- src/services/recipes.js
+++ src/services/recipes.js
@@ -12,19 +12,18 @@ const getAll = async (req, res) => {
 }
 
 const create = async (req, res) => {
-  // change later
-  const { title, servings, ingredients, instructions } = req.body
+  const { title, servings, ingredients, instructions, description, category, tags, difficulty, rating, minutes_to_make, notes, image, is_favorite } = req.body
   if (!title || !servings || !ingredients || !instructions) {
     return res.status(400).send('Required variables missing!')
   }
 
   try {
     const query = `
-      INSERT INTO recipes (title, servings, ingredients, instructions)
-      VALUES ($1, $2, $3, $4)
+      INSERT INTO recipes (title, servings, ingredients, instructions, description, category, tags, difficulty, rating, minutes_to_make, notes, image, is_favorite)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
       RETURNING *;
     `
-    const values = [title, servings, ingredients, instructions]
+    const values = [title, servings, ingredients, instructions, description, category, tags, difficulty, rating, minutes_to_make, notes, image, is_favorite]
     const { rows } = await db.query(query, values)
     if (rows.length === 0) {
       return res.status(404).send('No recipe found!')
@@ -54,11 +53,12 @@ const getById = async (req, res) => {
 const updateById = async (req, res) => {
   try {
     const { id } = req.params
-    const { title, servings, ingredients, instructions } = req.body
+    const { title, servings, ingredients, instructions, description, total_cost, category, tags, difficulty, rating, minutes_to_make, notes, image, is_favorite } = req.body
 
     // change this later to use Object.keys to pick columns
     // e.g. https://stackoverflow.com/questions/21759852/easier-way-to-update-data-with-node-postgres
 
+    // is this necessary?
     if (!title  || !servings || !ingredients || !instructions) {
       return res.status(400).send('Required variables missing!')
     }
@@ -68,11 +68,21 @@ const updateById = async (req, res) => {
       SET title = COALESCE($1, title),
           servings = COALESCE($2, servings),
           ingredients = COALESCE($3, ingredients),
-          instructions = COALESCE($4, instructions)
-      WHERE recipe_id = $5
+          instructions = COALESCE($4, instructions),
+          description = $5,
+          total_cost = $6,
+          category = $7,
+          tags = $8,
+          difficulty = $9,
+          rating = $10,
+          minutes_to_make = $11,
+          notes = $12,
+          image = $13,
+          is_favorite = $14
+      WHERE recipe_id = $15
       RETURNING *;
     `
-    const { rows } = await db.query(query, [title, servings, ingredients, instructions, id])
+    const { rows } = await db.query(query, [title, servings, ingredients, instructions, description, total_cost, category, tags, difficulty, rating, minutes_to_make, notes, image, is_favorite, id])
 
     if (rows.length === 0) {
       return res.status(404).send('No recipe found!')
